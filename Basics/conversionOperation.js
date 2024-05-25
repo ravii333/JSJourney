b118c6222de0eaddb29e1c6b22fa69008b355551
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-05-25 10:30:11 +0000

    Conversion Operations

--- Basics/conversionOperation.js
+++ Basics/conversionOperation.js
@@ -1,15 +1,15 @@
 let score = "30"
 let score2 = "30ab"
 
-console.log(typeof score)
-console.log(typeof(score2))
+//console.log(typeof score)
+//console.log(typeof(score2))
 
 let valueInNumber = Number(score) 
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 
 let valueInNumber2 = Number(score2)
-console.log(typeof valueInNumber2)
-console.log(valueInNumber2)
+//console.log(typeof valueInNumber2)
+//console.log(valueInNumber2)
 
 // "30" => 33
 // "30ab" => NaN (Not a Number)
@@ -17,8 +17,39 @@ console.log(valueInNumber2)
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 
 // 1 => true  0 => false
 // "" => false 
 // "ravi" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(typeof stringNumber)
+
+// **OPERATION**
+
+let value = 33
+let negValue = -value 
+// console.log(negValue)
+
+// Arithmetic 
+
+// console.log(3+3)
+// console.log(3-3)
+// console.log(3*3)
+// console.log(3**2) // power
+// console.log(3%3)
+
+let str1 = "Ravi"
+let str2 = " Kumar"
+let str3 = str1+str2
+// console.log(str3) // Ravi Kumar
+
+// console.log("1" + 3)  // 13
+// console.log(1 +"3")  // 13
+// console.log("1+1" + 3) // 1+13
+// console.log("1" + 3 + 3) // 133
+// console.log(1 + 1 + "3") // 23
+
+

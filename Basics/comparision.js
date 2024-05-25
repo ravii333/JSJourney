bdb314b9e82ed7465d880f45c0300ae6483b63f5
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-05-25 10:49:05 +0000

    Comparisions

---
+++ Basics/comparision.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+// console.log(2<=1)
+// console.log(2>=1)
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// Comparision check  and Equality  check works differently 
+
+// console.log(null > 0)
+
+// In comparision null convrted to number trealing it as 0
+
+// console.log(null == 0)  // false
+// console.log(null <= 0) // true
+// console.log(null >= 0) // true 
+
+console.log(undefined == 0)  // false
+console.log(undefined > 0) // false
+console.log(undefined >= 0) // false

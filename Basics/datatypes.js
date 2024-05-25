0b1728c1513fe0aaee2dedaf34a9e8c56cec0db2
Ravi Kumar <121314244+vira069@users.noreply.github.com>
2024-05-25 09:19:11 +0000

    Data types

---
+++ Basics/datatypes.js
@@ -0,0 +1,20 @@
+console.log("hello")
+
+const accountId = 12345
+let accountEmail = "Ravi@gamil.com"
+var accountPassword = "3445"
+
+accountCity = "Ranchi" 
+
+//accountId = 45 not allowed
+console.log(accountId)
+
+accountEmail="r@134"
+accountPassword="12121"
+accountCity = "Hyderabad"
+
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
